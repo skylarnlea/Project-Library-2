aeaf620437d1780ddd306f04ea03baf0fc698987
Skylar Lea <skyniclea@gmail.com>
2024-02-03 12:23:11 -0600

    adding spaces between functions

--- public/src/books.js
+++ public/src/books.js
@@ -2,15 +2,18 @@ function findAuthorById(authors, id) {
   return authors.find((author) => author.id === id);
 }
 
+
 function findBookById(books, id) {
   return books.find((book) => book.id === id);
 }
+
 
 function partitionBooksByBorrowedStatus(books) {
   const booksBorrowed = books.filter((book) => book.borrows[0] && !book.borrows[0].returned);
   const booksReturned = books.filter((book) => book.borrows[0] && book.borrows[0].returned);
   return [booksBorrowed, booksReturned];
 }
+
 
 function getBorrowersForBook(book, accounts) {
   const borrowers = book.borrows.map(borrow => {
